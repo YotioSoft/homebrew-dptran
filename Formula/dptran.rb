239288b2bc4f16b6ad073e164a26779f62d5db31
YotioSoft <ytani0323@gmail.com>
2022-10-22 00:49:31 +0900

    modify

--- Formula/dptran.rb
+++ Formula/dptran.rb
@@ -7,4 +7,9 @@ class Dptran < Formula
   url "https://github.com/YotioSoft/dptran/releases/download/ver.0.1.0/dptran-macos-arm64-ver.0.1.0.tar.gz"
   sha256 "69a76d2987d1442aee2d9c924e2ae6ffb99484d5d79825ba2566af1efcfef701"
   license ""
+  version "0.1.0"
+  
+  def install
+      bin.install "dptran"
+  end
 end
