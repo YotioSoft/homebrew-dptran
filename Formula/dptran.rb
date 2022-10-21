8c841b9df791876b8d537f6f2119c195aef57663
YotioSoft <ytani0323@gmail.com>
2022-10-22 00:25:58 +0900

    init

---
+++ Formula/dptran.rb
@@ -0,0 +1,11 @@
+class dptran < Formula
+  desc "A tool to run DeepL translations on command line."
+  homepage "https://github.com/YotioSoft/dptran.git"
+  url "https://github.com/YotioSoft/dptran/releases/download/ver.0.1.0/dptran-macos-arm64-ver.0.1.0.tar.gz"
+  sha256 "69a76d2987d1442aee2d9c924e2ae6ffb99484d5d79825ba2566af1efcfef701"
+  version "0.1.0"
+
+  def install
+    bin.install "dptran"
+  end
+end
